bcdd40ac7dc0fb94e8c383f58d01555d265450e7
Tan Hoang <manhtan12327@gmail.com>
2023-08-22 14:43:00 +0700

    finish

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -7,7 +7,9 @@ public class Rock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // make rock move in random direction
+        Vector3 direction = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10));
+        GetComponent<Rigidbody2D>().AddForce(direction, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
--- Assets/Scripts/RockSpawner.cs
+++ Assets/Scripts/RockSpawner.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class RockSpawner : MonoBehaviour
 {
+    [SerializeField]
+    GameObject prefabRock;
+
+    //value time 2 spawn a rock
+    float time2Spawn = 2;
+
+    //value 4 checking timer is running or not
+    bool isRuning = false;
+
+    //value 4 checking how many rock on the screen
+    int totalRock = 0;  
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        isRuning = true;     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isRuning)
+        {
+            if (time2Spawn > 0)
+            {
+                time2Spawn -= Time.deltaTime;
+            }
+            else
+            {
+                Instantiate(prefabRock);
+                totalRock += 1;
+                time2Spawn = 2;
+                Debug.Log(totalRock);
+            }
+        }
+        DestroyRock();
+    }
+
+    void DestroyRock()
+    {
+        if(totalRock == 4)
+        {
+            GameObject poorRock = GameObject.FindWithTag("Rock");
+            Destroy(poorRock);
+            totalRock = 3;
+        }
     }
 }
